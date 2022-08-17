d88b525063c900b9ddf5152f1cdfea6fa1742f30
Murali-MK <muralimk@outlook.com>
2022-08-17 12:58:13 +0530

    CRUD CoDE

---
+++ NgWithCore/API/Cards/Cards.API/Migrations/20220702145244_fieldnamechange.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cards.API.Migrations
+{
+    public partial class fieldnamechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpiryMonty",
+                table: "Cards",
+                newName: "ExpiryMonth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpiryMonth",
+                table: "Cards",
+                newName: "ExpiryMonty");
+        }
+    }
+}
